1c8e7f2023d7218bf3e2189b88c12699bc864951
Naia <43949390+NaiaIbanezdeGarayo@users.noreply.github.com>
2018-11-25 15:58:07 +0100

    Add files via upload

---
+++ Ejercicios Array/EjercicioArray9.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioarray9;
+
+/**
+ *
+ * @author Naia
+ */
+public class EjercicioArray9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int matriz [][][] = new int [10][3][4];
+        {
+            "ingles", "frances", "aleman", "ruso";
+        }
+                
+    }
+    
+}
