cc722980c6dfb21a8f250f0c9101189e448d46ff
Naia <43949390+NaiaIbanezdeGarayo@users.noreply.github.com>
2018-11-25 15:00:56 +0100

    Add files via upload

---
+++ Ejercicios Array/EjercicioArray8.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioarray8;
+
+import java.util.Arrays;
+import javax.swing.JOptionPane;
+
+
+public class EjercicioArray8 {
+
+    
+    public static void main(String[] args) {
+        int [][] variables = new int[10][10];
+        int fila = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de la fila"));
+        int columna = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de la columna"));
+        int n1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero"));
+        int n2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce otro numero"));
+        
+        do
+            {
+            int suma = n1 + n2;
+            }
+        while(fila== equals(variables));
+        }
+    }
+    
+}
